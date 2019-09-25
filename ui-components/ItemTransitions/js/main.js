21e9ab4d8aa49d6252fce441e5cd7919415d811d
Brian Jason <brian@jsdesigns.site>
2019-09-25 09:24:38 -0700

    finished

--- ui-components/ItemTransitions/js/main.js
+++ ui-components/ItemTransitions/js/main.js
@@ -75,13 +75,11 @@
 		var onEndAnimationNextItem = function() {
 			this.removeEventListener( animEndEventName, onEndAnimationNextItem );
 			classie.addClass( this, 'current' );
+
+			// controlling the height of the image's animated background shadow
 			var idName = $('.current img').attr('id');
 			var highness = $('#' + idName).height();
-			console.log(highness);
-			//		.fxPhotoBrowse li::before
-			////$('.fxPhotoBrowse li::before').height(highness + "px");
-			//		.fxPhotoBrowse li::after
-			////$('.fxPhotoBrowse li::after').height(highness + "px");
+			$('.fxPhotoBrowse li').height(highness + "px");
 
 			classie.removeClass( this, dir === 'next' ? 'navInNext' : 'navInPrev' );
 			++cntAnims;
